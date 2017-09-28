656330d790ceca6fa424591b86899065e649d785
AceWei <446839735@qq.com>
2017-09-28 11:45:30 +0800

    no message

--- tools.py
+++ tools.py
@@ -1,13 +1,6 @@
 import urllib, os
 
 class Deal:
-    def __init__(self):
-        self.path = DIR_PATH
-        if not self.path.endswith('/'):
-            self.path = self.path + '/'
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-
     # 保存网络图片（URL链接）
     def saveImgByUrl(url, filename):
         urllib.urlretrieve(url, filename)
@@ -40,35 +33,33 @@ class Deal:
     def getExtension(self, url):
         extension = url.split('.')[-1]
         extension = extension.split('?')[0]
-		return extension
+        return extension
 
-	# 获取当前时间戳
-	def ts():
-	    import time
-	    ticks = time.time()
-	    return int(ticks)
+    # 获取当前时间戳（秒）
+    def ts(self):
+        import time
+        ticks = time.time()
+        return int(ticks)
 
+    # base64加密编码
+    def base64encode(self, str):
+        # 编码
+        import base64
+        bytesString = str.encode(encoding="utf-8")
+        encodestr = base64.b64encode(bytesString).decode()
+        return encodestr
 
-	# MD5加密
-	def md5(str):
-	    import hashlib
-	    m = hashlib.md5()   
-	    m.update(str.encode('utf-8'))
-	    return m.hexdigest()
+    # base64解密解码
+    def base64decode(self, str):
+        # 解码
+        import base64
+        bytesString = str.encode(encoding="utf-8")
+        decodestr = base64.b64decode(bytesString)
+        return decodestr.decode()
 
-
-	# base64加密编码
-	def base64encode(str):
-	    # 编码
-	    import base64
-	    bytesString = str.encode(encoding="utf-8")
-	    encodestr = base64.b64encode(bytesString).decode()
-	    return encodestr
-
-	# base64解密解码
-	def base64decode(str):
-	    # 解码
-	    import base64
-	    bytesString = str.encode(encoding="utf-8")
-	    decodestr = base64.b64decode(bytesString)
-	    return decodestr.decode()
+    # MD5加密
+    def md5(str):
+        import hashlib
+        m = hashlib.md5()
+        m.update(str.encode('utf-8'))
+        return m.hexdigest()
